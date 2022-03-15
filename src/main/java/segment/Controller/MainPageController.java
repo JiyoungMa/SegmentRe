8a369dd319ef7f893fb9d30fb47c82c4106b47f4
DESKTOP-5836PEC\tiger <tiger980424@naver.com>
2022-03-15 15:36:05 +0900

    Chrome에서 오류 확인해서 고치기 중! 부트스트랩 적용이 안된다ㅠ

--- src/main/java/segment/Controller/MainPageController.java
+++ src/main/java/segment/Controller/MainPageController.java
@@ -4,15 +4,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Slf4j
 public class MainPageController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String mainPage(){
-        log.info("mainpage controller");
+        //log.info("mainpage controller");
         return "mainpage";
     }
 }
